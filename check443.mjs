49696b61d68b69e29f6c726900375c0260f54558
iiop123 <102747168+iiop123@users.noreply.github.com>
2024-07-13 12:00:30 +0800

    Add files via upload

---
+++ check443.mjs
@@ -0,0 +1,72 @@
+import IPCIDR from 'ip-cidr'
+import fs from "fs"
+import axios from 'axios';
+import {JSDOM} from 'jsdom'
+import {limitConcurrentRequests} from "./module/axios.mjs" 
+import {promisify} from "util"
+const writeFileAsync = promisify(fs.writeFile);
+const port = 443;
+const expectedStatusCode = 400;
+const htmlCheckContent = '<hr><center>cloudflare</center>';
+
+let iplist=[]
+
+async function checkIP(ip) {
+    try {
+        const url = `http://${ip}:${port}`;
+        let response = await limitConcurrentRequests(url, { validateStatus: status => status === expectedStatusCode, timeout: 3000 });
+		
+        if (response.status === expectedStatusCode) {
+		const dom = new JSDOM(response.data);
+		const document = dom.window.document;
+		const htmlContent = document.documentElement.innerHTML;
+		
+		if (htmlContent.includes(htmlCheckContent) && (response.headers.server==undefined||"cloudflare")) {
+			console.log(`IP ${ip} 返回状态码为 ${expectedStatusCode}，并且包含指定HTML内容`);
+			iplist.push(`${ip}/32\n`)
+			try {
+			    //await writeFileAsync('http400ips.json', JSON.stringify(iplist, null, 2));
+			    await writeFileAsync('./data/ips.txt', iplist.join(''));
+			} catch (error) {
+			    console.error('写入文件时出错:', error);
+			}
+		    
+		}	
+        }
+		
+    } catch (error) {
+			console.error(`无法连接到 ${ip}:${port}`);
+		
+        
+    }
+}
+
+function scanIPRange(ips){
+	ips.forEach(ip=>{
+		const cidr = new IPCIDR(ip);
+		const IpArray=cidr.toArray()
+		IpArray.forEach(checkIP)
+	})
+	
+	//ips.forEach(checkIP)
+}
+
+function getIPSegmentsFromFile(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(`Error reading the file: ${err}`);
+        return;
+      }
+
+      const ipSegments = data.split(/\r?\n/).filter(line => {
+        const ipRegex = /^(?:[0-9]{1,3}\.){3}[0-9]{1,3}\/(?:[0-9]|[1-2][0-9]|3[0-2])$/;
+        return ipRegex.test(line);
+      });
+
+      resolve(ipSegments);
+    });
+  });
+}
+
+scanIPRange(await getIPSegmentsFromFile('./data/testips.txt'))
---
+++ module/axios.mjs
@@ -0,0 +1,57 @@
+import axios from "axios"
+
+// 最大并发请求数
+const MAX_CONCURRENT_REQUESTS = 200; 
+
+// 请求队列
+const requestQueue = []; 
+
+// 当前正在进行中的请求数
+let activeRequests = 0; 
+
+/**
+ * 处理请求队列中的请求
+ */
+function processQueue() {
+  // 如果当前进行中的请求数没达到最大并发数 && 请求队列不为空
+  if (activeRequests < MAX_CONCURRENT_REQUESTS && requestQueue.length > 0) { 
+    // 从请求队列中取出队头的请求
+    const { url, config, resolve, reject } = requestQueue.shift(); 
+    // 进行中的请求数 +1
+    activeRequests++; 
+    // 通过 Axios 发送请求
+    axios(url, config) 
+    .then((response) => { 
+       // 请求成功，将外层 Promise 的状态更新为fulfilled，并返回请求结果
+       resolve(response); 
+     }) 
+    .catch((error) => { 
+       // 请求失败，将外层 Promise 的状态更新为rejected，并返回错误信息
+       reject(error); 
+     }) 
+    .finally(() => { 
+       // 不论成功还是失败都会执行 finally，表示本次请求结束，将进行中的请求数 -1
+       activeRequests--; 
+       // 再处理请求队列中的下一个请求
+       processQueue(); 
+     }); 
+  }
+}
+
+/**
+ * 并发请求方法
+ * @param {string} url 请求的 url
+ * @param {AxiosRequestConfig} config Axios 的请求配置
+ */
+function limitConcurrentRequests(url, config) { 
+  // 将用户发起的每个请求都变成一个 Promise，而 Promise 的状态会在 processQueue 中根据 Axios 的执行结果来更新
+  return new Promise((resolve, reject) => { 
+    // 将请求的配置信息和更新 Promise 状态的两个方法变成一个对象推入请求队列中
+    requestQueue.push({ url, config, resolve, reject }); 
+    // 执行 processQueue 方法处理请求队列中的每个请求
+    processQueue(); 
+  });
+}
+
+
+export {limitConcurrentRequests}
